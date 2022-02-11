7999a599484526c9cbf3bbe9a83ff7f6cf5e7fe9
w-cypriano <profwellington17@gmail.com>
2022-02-11 17:56:21 -0300

    Correçoes desafios

--- aula08-utilizando modulos/desafio19.py
+++ aula08-utilizando modulos/desafio19.py
@@ -1,12 +1,11 @@
 # Um professor quer sortear um dos seus quatro alunos para apagar o quadro. 
 # Faça um programa que ajude ele lendo o nome deles e escrevendo o nome do escolhido.
-import random
-from unittest import result
+from random import choice
 
 
 list_alunos = []
 while len(list_alunos) < 4:
     nome = input('digite o nomes dos alunos: ')
     list_alunos.append(nome)
-resultado = random.choices(list_alunos)
+resultado = choice(list_alunos)
 print('Alunos: {}'.format(resultado[0]))
